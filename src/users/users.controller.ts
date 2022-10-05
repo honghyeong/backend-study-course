a07c1eedd8f53610ab3c309709943c949065490f
honghyeong <scarletgold0601@gmail.com>
2022-10-05 21:43:36 +0900

    feat: assignment3 in-memory user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getUsers(): string[] {
-    return this.userService.getUsers();
+  fetchUsers() {
+    return this.usersService.fetchUsers();
+  }
+  @Get('/query')
+  fetchUsersByQuery(
+    @Query('offset') offset: number,
+    @Query('limit') limit: number,
+  ) {
+    return this.usersService.fetchUsersByQuery(offset, limit);
+  }
+
+  @Get('/:id')
+  fetchUser(@Param('id') id: number) {
+    return this.usersService.fetchUser(id);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: number): boolean {
+    return this.usersService.deleteUser(id);
+  }
+
+  @Put('/:id')
+  updateUser(
+    @Param('id') id: number,
+    @Body() updateInfo: { role: number; age: number },
+  ): boolean {
+    return this.usersService.updateUser(id, updateInfo);
   }
 }
